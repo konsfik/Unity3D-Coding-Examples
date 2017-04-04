1ba0db4190d306ae62b20fa090d0d724d39cb22c
Konstantinos Sfikas <kon.sfik@gmail.com>
2017-04-04 15:38:23 +0300

    did some more commenting, removed some unnecessary files and assets

--- 3-Procedural-Audio/ProceduralAudioUnityProject/Assets/Scripts/ComplexObjectShapeController.cs
+++ 3-Procedural-Audio/ProceduralAudioUnityProject/Assets/Scripts/ComplexObjectShapeController.cs
@@ -1,8 +1,18 @@
-﻿using System.Collections;
+﻿/*	Author: Kostas Sfikas
+	Date: April 2017
+	Language: c#
+	Platform: Unity 5.5.0 f3 (personal edition) */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExtrudedObjectShapeController : MonoBehaviour {
+public class ComplexObjectShapeController : MonoBehaviour {
+	/* This class controls the behaviour of the Complex Object. The data it uses comes from the
+	Procedural Audio Controller, so in a way this object is part of the Audio Controller's visualisation.
+	It does not have an actual functional role in the project, it is just a visual aid to help the 
+	user correlate som of the audio's behaviour to a visual analogue.*/
+
 	public ProceduralAudioController audioController;
 	public GameObject body;
 	public GameObject upPart;
@@ -12,11 +22,17 @@ public class ExtrudedObjectShapeController : MonoBehaviour {
 	public GameObject frontPart;
 	public GameObject backPart;
 
+	float xRotFactor;
+	float yRotFactor;
+	float zRotFactor;
+
 	// Use this for initialization
 	void Start () {
-		
+		xRotFactor = Random.Range (-50.0f, 50.0f);
+		yRotFactor = Random.Range (-50.0f, 50.0f);
+		zRotFactor = Random.Range (-50.0f, 50.0f);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		float scale = mapValue (audioController.amplitudeModulationRangeOut, 0.0f, 1.0f, 1.5f, 2.5f);
@@ -38,6 +54,10 @@ public class ExtrudedObjectShapeController : MonoBehaviour {
 		rightPart.transform.localScale = new Vector3 (freqScaleY,freqScaleXZ,freqScaleXZ);
 		frontPart.transform.localScale = new Vector3 (freqScaleXZ,freqScaleXZ,freqScaleY);
 		backPart.transform.localScale = new Vector3 (freqScaleXZ,freqScaleXZ,freqScaleY);
+
+		transform.Rotate(Vector3.right * Time.deltaTime * xRotFactor);
+		transform.Rotate(Vector3.up * Time.deltaTime * yRotFactor);
+		transform.Rotate(Vector3.forward * Time.deltaTime * zRotFactor);
 	}
 
 
