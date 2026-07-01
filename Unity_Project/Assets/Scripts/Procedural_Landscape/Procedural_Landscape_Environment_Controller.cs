25c1899cc5f8f1e1e5b1a9c1e7e1f202d244fdb8
DEVIL\konsf <kon.sfik@gmail.com>
2026-07-01 04:34:56 +0300

    merge three projects in one

---
+++ Unity_Project/Assets/Scripts/Procedural_Landscape/Procedural_Landscape_Environment_Controller.cs
@@ -0,0 +1,27 @@
+﻿/*	Author: Kostas Sfikas
+	Date: March 2017
+	Language: C#
+	Platform: Unity 5.5.0 f3 (personal edition) */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Procedural_Landscape_Environment_Controller : MonoBehaviour {
+	/* This class is of minor importance. It takes the bottomColor from the ProceduralLandscapeController
+	and applies it to the color of another material. This is used in the scene to make the color of some 
+	decorative objects the same as the landscape's bottom color, clearly for "aesthetic" reasons. */
+
+	public Material lineMaterial;
+	public Procedural_Landscape_Controller landscapeController;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lineMaterial.color = landscapeController.bottomColor;
+	}
+}
